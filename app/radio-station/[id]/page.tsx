329ffd71b9058f0edf853fc95d0292d7629af9af
Shadi <github-iro7msidew4z@revar.net>
2025-02-10 19:49:02 +0100

    remove docker for now

--- app/radio-station/[id]/page.tsx
+++ app/radio-station/[id]/page.tsx
@@ -1,47 +1,22 @@
-// 'use client';
-// import { useState, useEffect } from 'react';
-import type { RadioApp } from '@/business/type/radio/radioApp';
-import { getRadioStation } from '@/business/store/radioStore';
+import { getRadioStation, getRadioStations } from '@/business/store/radioStore';
 import RadioDetail from '@/presentation/component/RadioDetail';
 import Page from '@/presentation/component/Page';
 
-// getRadioStations
-// export async function generateStaticParams() {
-//     const radioStations = await getRadioStations();
+export async function generateStaticParams() {
+    const radioStations = await getRadioStations();
 
-//     if (!radioStations) {
-//         return [];
-//     }
+    if (!radioStations) {
+        return [];
+    }
 
-//     return radioStations.map((post) => ({
-//         id: post.id.toString(),
-//     }));
-// }
+    return radioStations.map((post) => ({
+        id: post.id,
+    }));
+}
 
 export default async function RadioStationPage({ params }: { params: Promise<{ id: string }> }) {
     const id = (await params).id;
     const radioStation = await getRadioStation(id);
 
-    // const params = useParams();
-
-    // const [radioStation, setRadioStation] = useState<RadioApp | null>(null);
-
-    // useEffect(() => {
-    //     (async () => {
-    //         const radio = await getRadioStation(params.id);
-
-    //         if (!radio) {
-    //             return;
-    //         }
-
-    //         setRadioStation(radio);
-    //     })();
-    // }, []);
-
-    return (
-        <Page>
-            {/* <button onClick={() => window.history.back()}>Back</button> */}
-            {radioStation && <RadioDetail radio={radioStation} />}
-        </Page>
-    );
+    return <Page>{radioStation && <RadioDetail radio={radioStation} />}</Page>;
 }
